76c9519cc3587e68902e259d067a83d255515456
Anastasia <anastasia.koles@gmail.com>
2023-10-18 17:10:09 +0300

    the task for HW 1 was completed

---
+++ HW_1/Task1.py
@@ -0,0 +1,19 @@
+'''
+Дан список целых чисел numbers. Необходимо написать в императивном стиле процедуру для
+сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки.
+'''
+
+
+def selection_sort(nums):
+    for number in range(len(nums)):
+        lowest_value_index = number
+        for j in range(number + 1, len(nums)):
+            if nums[j] < nums[lowest_value_index]:
+                lowest_value_index = j
+        nums[number], nums[lowest_value_index] = nums[lowest_value_index], nums[number]
+
+if __name__ == "__main__":
+    random_list_of_nums = [12, 8, 67, 34,  3, 20, 11]
+    selection_sort(random_list_of_nums)
+    random_list_of_nums.reverse()
+    print(random_list_of_nums)
